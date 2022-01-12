ef275ea861a7d154df8abe0bd5e1770d314e1bf0
李子航 <2045268468@qq.com>
2022-01-12 11:00:22 +0800

    shop页面navbar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,35 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-const routes: Array<RouteRecordRaw> = []
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/shop'
+  },
+  {
+    path: '/shop',
+    component: () => import('views/shop.vue')
+  },
+  {
+    path: '/category',
+    component: () => import('views/category.vue')
+  },
+  {
+    path: '/cart',
+    component: () => import('views/cart.vue')
+  },
+  {
+    path: '/consult',
+    component: () => import('views/consult.vue')
+  },
+  {
+    path: '/login',
+    component: () => import('views/login.vue')
+  },
+  {
+    path: '/prefile',
+    component: () => import('views/prefile.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/shop.vue
@@ -0,0 +1,37 @@
+<template>
+  <nut-navbar
+    class="navbar"
+    title="商城"
+    icon="my2"
+    @on-click-back="back"
+    @on-click-send="send"
+  ></nut-navbar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core'
+import { useRouter } from 'vue-router'
+export default defineComponent({
+  name: 'Shop',
+  setup() {
+    const router = useRouter()
+    function back() {
+      router.back()
+    }
+    function send() {
+      router.push('/prefile')
+    }
+    return {
+      back,
+      send
+    }
+  }
+})
+</script>
+
+<style scoped lang="less">
+.navbar {
+  margin: 0;
+  height: 60px;
+}
+</style>
