dbbbac349a00a99abace8f5bcdd34d089c4b3f4c
李子航 <2045268468@qq.com>
2022-01-21 10:23:02 +0800

    与服务器跨域，设置代理

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   outputDir: './build',
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8000',
+        pathRewrite: {
+          '^/api': ''
+        },
+        changeOrigin: true
+      }
+    }
+  },
   configureWebpack: {
     resolve: {
       alias: {
