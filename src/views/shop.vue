7081aae05d77568db78f235ff280d609ad01666e
李子航 <2045268468@qq.com>
2022-01-26 14:42:34 +0800

    首页样式第一版

--- src/views/shop.vue
+++ src/views/shop.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="shop">
     <nav-bar title="商城" />
+    <!-- 搜索框 -->
+    <van-search
+      class="search"
+      v-model="value"
+      placeholder="请输入搜索关键词"
+      @click-input="onSearch"
+    />
 
     <main class="main">
-      <!-- 搜索框 -->
-      <van-search
-        class="search"
-        v-model="value"
-        placeholder="请输入搜索关键词"
-        @click-input="onSearch"
-      />
-
       <!-- 轮播图 -->
       <van-swipe class="swiper" :autoplay="3000" indicator-color="white">
         <van-swipe-item v-for="item in swiper.list" :key="item">
           <img class="img" v-lazy="item" alt="" />
         </van-swipe-item>
       </van-swipe>
+
+      <!-- 商品列表 -->
+      <shop-list class="list" :search-list="searchResult.list"></shop-list>
     </main>
   </div>
 </template>
@@ -26,9 +28,11 @@ import { defineComponent, ref, reactive } from 'vue'
 import { useRouter } from 'vue-router'
 import NavBar from '@/components/NavBar.vue'
 import { getSwiper } from '@/api/shop'
+import { searchAll } from '@/api/search'
+import ShopList from '@/components/ShopList.vue'
 
 export default defineComponent({
-  components: { NavBar },
+  components: { NavBar, ShopList },
   name: 'Shop',
   setup() {
     // 搜索框
@@ -45,10 +49,18 @@ export default defineComponent({
     getSwiper().then((res: any) => {
       swiper.list = res
     })
+
+    // 请求全部商品数据
+    const searchResult: { list: any[] } = reactive({ list: [] })
+    searchAll().then((res: any) => {
+      searchResult.list = res
+    })
+
     return {
       value,
       onSearch,
-      swiper
+      swiper,
+      searchResult
     }
   }
 })
@@ -58,21 +70,22 @@ export default defineComponent({
 .shop {
   width: 750px;
   height: calc(100% - 100px);
+  background-color: #f5f5f5;
+
+  .search {
+    width: 100%;
+    padding: 10px 20px;
+    margin-bottom: 10px;
+  }
 
   .main {
-    height: calc(100% - 80px);
+    height: calc(100% - 178px);
     padding: 0 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    .search {
-      width: 100%;
-      padding: 20px 0;
-    }
+    overflow: auto;
 
     .swiper {
       width: 100%;
+      height: 266px;
       border-radius: 20px;
       .img {
         width: 100%;
