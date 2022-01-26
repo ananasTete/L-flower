7c8f83fb95315fa2a756498bbb40f4476f0a896a
李子航 <2045268468@qq.com>
2022-01-26 14:48:23 +0800

    修改商品展示列表组件

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,9 +1,16 @@
 import request from '@/network/request'
 
+// 搜索匹配flower信息
 function search(value: string) {
   return request.get({
     url: `/flower/search?q=${value}`
   })
 }
 
-export { search }
+// 获取所有flower信息
+function searchAll() {
+  return request.get({
+    url: `/flower`
+  })
+}
+export { search, searchAll }
---
+++ src/components/ShopList.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="shop-list">
+    <template v-for="item in searchList" :key="item.id">
+      <div class="item">
+        <div class="img-div">
+          <img :src="item.img_url[0]" alt="" class="img" />
+        </div>
+        <div class="item-content">
+          <div class="title">{{ item.name }}</div>
+          <div class="introduce">{{ item.material }}</div>
+          <div class="price">
+            售价: <span class="red-price">￥{{ item.price }}</span>
+          </div>
+          <div class="count">月销:{{ item.soldCount }}</div>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  name: '',
+  props: {
+    searchList: {
+      type: Array as PropType<any[]>
+    }
+  }
+})
+</script>
+
+<style scoped lang="less">
+// 不设置高度，由item撑开
+.shop-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  .item {
+    width: 49%;
+    height: 500px;
+    border-radius: 20px;
+    margin: 10px 0;
+    overflow: hidden;
+    background-color: #fff;
+
+    .img-div {
+      width: 100%;
+      height: 350px;
+      // overflow: hidden;
+
+      .img {
+        width: 100%;
+      }
+    }
+
+    .item-content {
+      height: 150px;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: 1fr 1fr 1fr;
+      font-size: 25px;
+      padding: 0 10px;
+
+      .title {
+        grid-column-start: 1;
+        grid-column-end: 3;
+        font-size: 30px;
+        font-weight: 500;
+        align-self: center;
+      }
+
+      .introduce {
+        grid-column-start: 1;
+        grid-column-end: 3;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        color: #808080;
+      }
+      .red-price {
+        color: #db4453;
+      }
+      .count {
+        justify-self: end;
+      }
+    }
+  }
+}
+</style>
